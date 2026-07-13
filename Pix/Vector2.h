fe0ae902ed547fb8dcb12d2bfc9c080bf1c781a4
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-07-12 23:59:22 -0700

    Add Math structs

--- Pix/Vector2.h
+++ Pix/Vector2.h
@@ -1,1 +1,45 @@
 #pragma once
+
+struct Vector2
+{
+	float x, y;
+
+	Vector2() : x(0.0f), y(0.0f) {}
+	Vector2(float s) : x(s), y(s) {}
+	Vector2(float x, float y) : x(x), y(y) {}
+	Vector2(const Vector2& v) : x(v.x), y(v.y) {}
+
+	// common operators
+	Vector2 operator-() const
+	{
+		return { -x, -y };
+	}
+
+	Vector2 operator+(const Vector2& rhs) const
+	{
+		return { x + rhs.x, y * rhs.y };
+	}
+	Vector2 operator-(const Vector2& rhs) const
+	{
+		return { x - rhs.x, y - rhs.y };
+	}
+	Vector2 operator*(const Vector2& rhs) const
+	{
+		return { x * rhs.x, y * rhs.y };
+	}
+	Vector2 operator/(const Vector2& rhs) const
+	{
+		return { x / rhs.x, y / rhs.y };
+	}
+
+
+
+	// common inline operators
+	Vector2& operator+=(const Vector2& rhs) { x += rhs.x; y += rhs.y; return *this; }
+	Vector2& operator-=(const Vector2& rhs) { x -= rhs.x; y -= rhs.y; return *this; }
+	Vector2& operator*=(float s) { x *= s; y *= s; return *this; }
+	Vector2& operator/=(float s) { x /= s; y /= s; return *this; }
+
+
+
+};
--- Pix/Vector3.h
+++ Pix/Vector3.h
@@ -1,1 +1,45 @@
 #pragma once
+
+struct Vector3
+{
+	float x, y, z;
+
+	Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
+	Vector3(float s) : x(s), y(s), z(s) {}
+	Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+	Vector3(const Vector3& v) : x(v.x), y(v.y), z(v.z) {}
+
+	// common operators
+	Vector3 operator-() const
+	{
+		return { -x, -y , -z };
+	}
+
+	Vector3 operator+(const Vector3& rhs) const
+	{
+		return { x + rhs.x, y + rhs.y, z + rhs.z };
+	}
+	Vector3 operator-(const Vector3& rhs) const
+	{
+		return { x - rhs.x, y - rhs.y , z - rhs.z };
+	}
+	Vector3 operator*(const Vector3& rhs) const
+	{
+		return { x * rhs.x, y * rhs.y , z * rhs.z };
+	}
+	Vector3 operator/(const Vector3& rhs) const
+	{
+		return { x / rhs.x, y / rhs.y , z / rhs.z };
+	}
+
+
+
+	// common inline operators
+	Vector3& operator+=(const Vector3& rhs) { x += rhs.x; y += rhs.y; z += rhs.z; return *this; }
+	Vector3& operator-=(const Vector3& rhs) { x -= rhs.x; y -= rhs.y; z -= rhs.z; return *this; }
+	Vector3& operator*=(float s) { x *= s; y *= s; z *= s; return *this; }
+	Vector3& operator/=(float s) { x /= s; y /= s; z /= s; return *this; }
+
+
+
+};
